04990a4feb6066c6b5ced2eb4a12db791dbb4b70
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2024-11-21 19:39:26 +0530

    Fixed small ssr issue

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -1,42 +1,42 @@
-import { flag } from '@vercel/flags/sveltekit';
+// import { flag } from '@vercel/flags/sveltekit';
  
-export const landingDepDiv1Flag = flag<boolean>({
-  key: 'landingDepDiv1Flag',
-//   origin: 'https://example.com/#showdashbord', // optional
-  options: [{ value: true }, { value: false }], // optional
-  // can be async and has access to the event
-  decide(_event) {
-    return false;
-  },
-});
+// export const landingDepDiv1Flag = flag<boolean>({
+//   key: 'landingDepDiv1Flag',
+// //   origin: 'https://example.com/#showdashbord', // optional
+//   options: [{ value: true }, { value: false }], // optional
+//   // can be async and has access to the event
+//   decide(_event) {
+//     return false;
+//   },
+// });
 
-export const landingDepDiv2Flag = flag<boolean>({
-    key: 'landingDepDiv2Flag',
-  //   origin: 'https://example.com/#showdashbord', // optional
-    options: [{ value: true }, { value: false }], // optional
-    // can be async and has access to the event
-    decide(_event) {
-      return false;
-    },
-  });
+// export const landingDepDiv2Flag = flag<boolean>({
+//     key: 'landingDepDiv2Flag',
+//   //   origin: 'https://example.com/#showdashbord', // optional
+//     options: [{ value: true }, { value: false }], // optional
+//     // can be async and has access to the event
+//     decide(_event) {
+//       return false;
+//     },
+//   });
   
 
-export const landingDepDiv3Flag = flag<boolean>({
-key: 'landingDepDiv3Flag',
-//   origin: 'https://example.com/#showdashbord', // optional
-options: [{ value: true }, { value: false }], // optional
-// can be async and has access to the event
-decide(_event) {
-    return false;
-},
-});
+// export const landingDepDiv3Flag = flag<boolean>({
+// key: 'landingDepDiv3Flag',
+// //   origin: 'https://example.com/#showdashbord', // optional
+// options: [{ value: true }, { value: false }], // optional
+// // can be async and has access to the event
+// decide(_event) {
+//     return false;
+// },
+// });
 
-export const landingDepDiv4Flag = flag<boolean>({
-key: 'landingDepDiv4Flag',
-//   origin: 'https://example.com/#showdashbord', // optional
-options: [{ value: true }, { value: false }], // optional
-    // can be async and has access to the event
-    decide(_event) {
-      return false;
-    },
-  });
+// export const landingDepDiv4Flag = flag<boolean>({
+// key: 'landingDepDiv4Flag',
+// //   origin: 'https://example.com/#showdashbord', // optional
+// options: [{ value: true }, { value: false }], // optional
+//     // can be async and has access to the event
+//     decide(_event) {
+//       return false;
+//     },
+//   });
