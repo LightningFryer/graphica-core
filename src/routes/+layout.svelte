94c9411b2e6600f399acca17fc3012fe3dc3f6e3
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2024-12-09 21:35:35 +0530

    Added view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	import ViewTransition from "$lib/components/Navigation.svelte";
+
 </script>
 
+<ViewTransition />
 {@render children()}
