46fbfc0a1dbefd3b5eb86f087ca720d59917482d
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2024-12-11 22:26:40 +0530

    fix for vercel deployment

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -17,22 +17,21 @@
 </main>
 
 <style>
-    .team-sec {
-		background-image: url("$lib/images/DotGrid.png");
-		background-size: auto;
-		background-repeat: repeat;
-		animation: moveBg 40s linear infinite;
+   .team-sec {
+		position: relative;
+		width: 100%;
+		height: 100vh; /* Full viewport height */
+		background: radial-gradient(circle, #c39f61 1px, transparent 1px);
+		background-size: 20px 20px; /* Spacing between dots */
+		animation: moveDots 3s linear infinite;
 	}
 
-	@keyframes moveBg{
-		from{
-			background-position-y: 0%;
-			background-position-x: 0%;
+	@keyframes moveDots {
+		0% {
+		background-position: 0 0;
 		}
-
-		to{
-			background-position-y: 100%;
-			background-position-x: -100%;
+		100% {
+		background-position: 20px -20px;
 		}
 	}
 </style>
