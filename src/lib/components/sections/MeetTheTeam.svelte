e1ade4f610ac7fe7e651927bda82eb72046b1d37
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2025-01-31 21:17:11 +0530

    changed font size of another team card

--- src/lib/components/sections/MeetTheTeam.svelte
+++ src/lib/components/sections/MeetTheTeam.svelte
@@ -229,7 +229,7 @@
 				class="flex h-full rotate-180 flex-col justify-end p-2 backdrop-blur-[0.1rem] backdrop-brightness-95 [writing-mode:vertical-lr] md:p-4"
 			>
 				<p class="font-poppins text-xl font-extralight italic md:text-2xl">3D Designer</p>
-				<p class="font-poppins text-2xl font-bold italic md:text-3xl">Alankryt Sharma</p>
+				<p class="font-poppins text-xl font-bold italic md:text-3xl">Alankryt Sharma</p>
 			</div>
 		</div>
 		<div
