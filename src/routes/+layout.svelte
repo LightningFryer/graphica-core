a0d044f196ca5c1ffb7bb7f1d219c072bc5a4eb2
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2025-03-06 18:23:17 +0530

    Added new meta SEO tags for screen readers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,19 @@
 </script>
 
 <svelte:head>
-	<meta
+	<!-- <meta
 		name="description"
 		content="Explore the world of cutting-edge design at Club Graphica. Learn, create, and share!"
 	/>
 	<meta name="keywords" content="design, graphic, tech, tutorials" />
-	<meta name="author" content="Club Graphica Team" />
+	<meta name="author" content="Club Graphica Team" /> -->
+
+	<meta property="og:site_name" content="Club Graphica" />
+	<meta property="og:title" content="Club Graphica" />
+	<meta
+		property="og:description"
+		content="Explore the world of cutting-edge design at Club Graphica. Learn, create, and share!"
+	/>
 </svelte:head>
 
 <ViewTransition />
